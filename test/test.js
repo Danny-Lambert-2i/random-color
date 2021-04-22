062d55313b18e03962393fe8b599642f395427d1
Douglas MacIntyre <douglas.macintyre@2itesting.com>
2021-04-22 14:27:18 +0100

    changes on dm
    changes

---
+++ test/test.js
@@ -0,0 +1,16 @@
+let assert = require('assert');
+
+describe('Basic mocha test', function(){
+    it('should throw errors', function(){
+        assert.strictEqual(2,3);
+    })
+})
+
+describe('Hex value test'), () => {
+    it('should validate true hex values', () => {
+        assert.strictEqual(
+            // random character from generated hex
+            // list of valid characters
+        )
+    })
+}
